bf7d52b6490a992cd20a60e6633c7a2da202f021
juliansarmien18 <julian010422@gmail.com>
2021-06-17 14:08:31 -0500

    Se crea funcion adicionar al reto 5

--- Reto5/Reto5.py
+++ Reto5/Reto5.py
@@ -6,6 +6,24 @@ def menu():
     print('4.Listar total de beneficiarios')
     print('5.Listar beneficiarios segun letra determinada')
     print('0.Salir')
+    op = int(input('Usted escoge la opcion:'))
+    return op
+
+def Adicionar():
+    entradaLogico=open('agenda.txt', 'r')
+    Nombre=input('digite nombre del nuevo usuario:')
+    cedula=input('Digite cedula del nuevo beneficiario:')
+    for linea in entradaLogico:
+        while linea == cedula+'\n':
+            cedula = input('esta cedula ya existe, digite una nueva: ')
+    entradaLogico.close()
+    tel= input('Digite numero de telefono del nuevo beneficiario: ')
+    entradaLogico = open('agenda.txt','a')
+    entradaLogico.write('\n'+'\n'+Nombre)
+    entradaLogico.write('\n'+cedula)
+    entradaLogico.write('\n'+tel)
+    entradaLogico.close()
+    print('se adiciono el nuevo beneficiario')
 
 def ListaCompleta():
     entradaLogico = open('agenda.txt', 'r')
@@ -14,17 +32,24 @@ def ListaCompleta():
     print()
     entradaLogico.close()
 
-menu()
-op = int(input('Usted escoge la opcion:'))
-if op == 1:
-    print('holi')
-elif op == 2:
-    entradaLogico = open('agenda.txt', 'a')
-elif op == 3:
-    print('holi')
-elif op == 4:
-    ListaCompleta()
-elif op == 5:
-    print('holi')
-else:
-    exit()
+op=menu()
+while op != 0:
+    if op == 1:
+        print('holi')
+        op=menu()
+    elif op == 2:
+        Adicionar()
+        print()
+        op=menu()
+    elif op == 3:
+        print('holi')
+        op=menu()
+    elif op == 4:
+        ListaCompleta()
+        print()
+        op=menu()
+    elif op == 5:
+        print('holi')
+        op=menu()
+    else:
+        exit()
