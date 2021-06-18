3b7b15572a3d2a482655a5b7f984f3277695b356
juliansarmien18 <julian010422@gmail.com>
2021-06-17 21:33:35 -0500

    retocado para presentar en la plataforma

--- Reto5/Reto5.py
+++ Reto5/Reto5.py
@@ -1,47 +1,52 @@
 from os import read, write
 
+
 def menu():
-    print('BIENVENIDO/A'+'\n'+'Elija Una de las Siguiente Opciones')
-    print('1. Buscar numero de telefono segun nombre y apellido')
-    print('2. Adicionar un nuevo beneficiario')
-    print('3. Borrar un beneficiario de la lista')
-    print('4.Listar total de beneficiarios')
-    print('5.Listar beneficiarios segun letra determinada')
-    print('0.Salir')
-    op = int(input('Usted escoge la opcion:'))
+    print('Menu Principal')
+    print('1. Ver listado')
+    print('2. Ver listado filtrado')
+    print('3. Agregar beneficiario')
+    print('4. Buscar beneficiario')
+    print('5. Borrar beneficiario')
+    print('6. Salir')
+    op = int(input())
     return op
 
 def BuscarNom():
-    Datos = input('Digite el nombre y el apellido de la persona  buscar el numero:')
-    entradaLogico = open('agenda.txt','r')
-    for linea in entradaLogico:
-        linea = linea.rstrip()
-        if linea == Datos:
-            cel = entradaLogico.readlines()[1]
-            entradaLogico.close
-            print('El telefono de',Datos,'es: ',cel)
-    entradaLogico.close()
+    Datos = input('Digite el nombre y apellido del beneficiario a buscar:')
+    f=open("agenda.txt","r")
+    lineas = f.readlines()
+    for n in lineas:
+        if n==(Datos+'\n'):
+            cedula=[lineas[lineas.index(n)+1]]
+            cel=[lineas[lineas.index(n)+2]]
+            print()
+            print(n,end="")
+            print(cedula[0],end="")
+            print(cel[0])
+            break
+    f.close()
 
 def Adicionar():
     entradaLogico=open('agenda.txt', 'r')
-    Nombre=input('digite nombre del nuevo usuario:')
-    cedula=input('Digite cedula del nuevo beneficiario:')
-    #comparar variable con archivo txt
+    print('Digite la información del beneficiario a agregar:')
+    Nombre=input()
+    cedula=input()
     for linea in entradaLogico:
         linea = linea.rstrip()
         while linea == cedula:
             cedula = input('esta cedula ya existe, digite una nueva: ')
     entradaLogico.close()
-    tel= input('Digite numero de telefono del nuevo beneficiario: ')
+    tel= input()
     entradaLogico = open('agenda.txt','a')
-    entradaLogico.write('\n'+'\n'+Nombre)
+    entradaLogico.write('\n'+Nombre)
     entradaLogico.write('\n'+cedula)
     entradaLogico.write('\n'+tel)
     entradaLogico.close()
-    print('se adiciono el nuevo beneficiario')
+    print('El beneficiario ha sido agregado')
 
 def Borrar():
-    cedula = input('Digite la cedula del beneficiario a Borrar:')
+    cedula = input('Digite la cedula del beneficiario a borrar:')
     with open("agenda.txt","r") as f:
         lineas = f.readlines()
         numero = [lineas[lineas.index(n)+1]for n in lineas if n==(cedula+'\n')]
@@ -54,47 +59,51 @@ def Borrar():
             if linea != (Nombre[0]) and linea != (numero[0]) and linea != (cedula+'\n'):
                 f.write(linea)
         f.close()
-        print('Se ha borrado con exito el Beneficiario')
+        print()
+        print("El usuario ha sido eliminado del listado")
 
 def ListaCompleta():
     entradaLogico = open('agenda.txt', 'r')
+    print("Listado de beneficiarios",end="")
     for linea in entradaLogico:
-        print(linea,end="")
-    print()
+        linea=linea.rstrip()
+        print(linea)
     entradaLogico.close()
 
 def PorLetra():
-    letra = input('Digite por favor la letra mayuscula a filtrar: ')
+    letra = input('Digite la letra por la que empiezan los beneficiarios:')
     f=open("agenda.txt","r")
     lineas = f.readlines()
+    print()
+    print("Listado filtrado de beneficiarios:")
+    print(end="")
     for n in lineas:
         if n[0]==letra:
             cedula=[lineas[lineas.index(n)+1]]
             cel=[lineas[lineas.index(n)+2]]
-            
+            print(end="")
             print(n,end="")
             print(cedula[0],end="")
-            print(cel[0])
+            print(cel[0],end="")
     f.close()
 
+f = open('agenda.txt','a')
+f.close()
 op=menu()
-while op != 0:
+while op != 6:
     if op == 1:
-        BuscarNom()
+        ListaCompleta()
         op=menu()
     elif op == 2:
-        Adicionar()
-        print()
-        op=menu()
-    elif op == 3:
-        Borrar()
-        op=menu()
-    elif op == 4:
-        ListaCompleta()
-        print()
-        op=menu()
-    elif op == 5:
         PorLetra()
         op=menu()
-    else:
-        exit()
+    elif op == 3:
+        Adicionar()
+        op=menu()
+    elif op == 4:
+        BuscarNom()
+        op=menu()
+    elif op == 5:
+        Borrar()
+        op=menu()
+print("Hasta pronto")
